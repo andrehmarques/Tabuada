a8ab830b8afcb12d9564d2bf3f3af6741be9e16e
Andreh Marques <andrehmarquez@gmail.com>
2025-06-02 21:34:36 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int numero;
+
+Console.WriteLine("-- Tabuada.exe --");
+
+Console.WriteLine("Digite o número que você deseja saber a tabuada: ");
+while (!int.TryParse(Console.ReadLine()!, out numero))
+{
+    Console.WriteLine("Digite um número válido");
+}
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"{numero} x {i} = {numero * i}");
+}
+
+Console.WriteLine("Digite uma tecla qualquer para finalizar o programa!");
+Console.ReadKey();
